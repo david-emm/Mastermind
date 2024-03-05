7e5fc918862b95233ddca5ece24802f3ead744d6
david@skea.com <david@skea.com>
2024-03-05 11:58:23 +1100

    Better input routine

--- mastermind.py
+++ mastermind.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3hints
 
 """
 This is a text based version of MasterMind written in Python 3.
@@ -41,9 +41,9 @@ sys.stdout.write(f"\x1b[8;{40};{100}t")
 print("\nThis is a text based version of MasterMind written in Python 3.")
 print("Emojis are used instead of coloured pegs.")
 print("After your guess the program will help you with the following hints")
-print("  ⚫ - Black. A colour is present and is correctly placed. ")
-print("  ⚪ - White. A colour is present but in the wrong place.")
-print("  🟤 - Brown. A colour is not in the answer.")
+print("  ⚫ - Black. A colour that is present and is correctly placed. ")
+print("  ⚪ - White. A colour that is present but in the wrong place.")
+print("  🟤 - Brown. A colour that is not in your guess.")
 print("These help pegs do not indicate the position of the guess pegs.\n")
 print("Enter 4 colours - back space may be used to correct entry.")
 
@@ -65,13 +65,14 @@ while playagain:
 			choice = input("Enter: r for 🔴, o for 🟠, g for 🟢, b for 🔵, p for 🟣, y for 🟡: " )
 			if len(choice) != 4:
 				print("Enter 4 and only 4 colours.")
-				continue
-			for i in range(4):
-				if choice[i] not in ("rogbpy") or choice == '':
-					print("You must only enter r, o, g, b, p or y")
-				else:
-					return choice 
-				
+			else:
+				if len(choice) == 4:
+					contained: str = [x for x in choice if x in "rogbpy"]	
+					if len(contained) == 4:
+						return choice
+					else:
+						print("You must only enter r, o, g, b, p or y")
+
 	def answer(hidden: list[str], guess: list[str]) -> list[str]:
 		myanswer: list[str]= []
 		a: list[str] = hidden
@@ -118,9 +119,9 @@ while playagain:
 
 		# Computer's prompt to help player to make better guess.
 		myprompt = answer(mychoice, yourchoice)
+		random.shuffle(myprompt)
 
 		# Printing guesses and prompts and the next guess   
-		# ~ print(f"\nThis is your quess {guess}")
 		print(f"Guess {guess}: Your choice: {yourchoice[0]}, {yourchoice[1]}, {yourchoice[2]}, {yourchoice[3]}, | My prompt is {myprompt[0]}, {myprompt[1]}, {myprompt[2]}, {myprompt[3]}\n")
 		
 		# Check for win
